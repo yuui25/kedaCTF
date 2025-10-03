7268d3a191b87ff2af6572a08e946e098a3d946b
Yuui <fujizakk.southernrock25@gmail.com>
2025-10-04 02:22:46 +0900

    Update ctf-down.sh

--- 02_Ops_Package/scripts/ctf-down.sh
+++ 02_Ops_Package/scripts/ctf-down.sh
@@ -6,7 +6,7 @@ if docker compose version >/dev/null 2>&1; then
 elif command -v docker-compose >/dev/null 2>&1; then
   docker-compose down
 else
-  echo "docker compose / docker-compose のいずれも見つかりません"
+  echo "docker compose / docker-compose のいずれも見つかりません（停止不要か、未インストールの可能性）"
   exit 1
 fi
 
